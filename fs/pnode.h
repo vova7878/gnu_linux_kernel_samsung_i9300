4cb20ec4e320b9aa84cfce0c23aaef47656e1900
Al Viro <viro@zeniv.linux.org.uk>
2011-11-24 21:28:22 -0500

    vfs: spread struct mount mnt_set_mountpoint child argument
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/pnode.h
+++ fs/pnode.h
@@ -40,7 +40,7 @@ bool is_path_reachable(struct vfsmount *, struct dentry *,
 			 const struct path *root);
 unsigned int mnt_get_count(struct vfsmount *mnt);
 void mnt_set_mountpoint(struct vfsmount *, struct dentry *,
-			struct vfsmount *);
+			struct mount *);
 void release_mounts(struct list_head *);
 void umount_tree(struct mount *, int, struct list_head *);
 struct mount *copy_tree(struct mount *, struct dentry *, int);
