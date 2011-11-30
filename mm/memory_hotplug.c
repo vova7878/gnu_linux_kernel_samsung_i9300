f3388290632a10993f05c5222be234ae43c3da22
Jack Cheung <jackc@codeaurora.org>
2011-11-29 16:53:34 -0800

    mm: Fix zone->present_pages underflow
    If offlined_pages is greater than
    zone->present_pages, underflow will occur.
    
    This change will set zone->present_pages to 0 if
    offlined_pages is greater.
    
    Change-Id: I728e90c60fb7fc391de7b9c4828ab264ca38653b
    Signed-off-by: Jack Cheung <jackc@codeaurora.org>

--- mm/memory_hotplug.c
+++ mm/memory_hotplug.c
@@ -975,7 +975,10 @@ repeat:
 	undo_isolate_page_range(start_pfn, end_pfn, MIGRATE_MOVABLE);
 #endif
 	/* removal success */
-	zone->present_pages -= offlined_pages;
+	if (offlined_pages > zone->present_pages)
+		zone->present_pages = 0;
+	else
+		zone->present_pages -= offlined_pages;
 	zone->zone_pgdat->node_present_pages -= offlined_pages;
 	totalram_pages -= offlined_pages;
 
