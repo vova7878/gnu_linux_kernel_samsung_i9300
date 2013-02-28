04be5e4f76f8f24cdf705db32b493877ba22bece
Andrey Vagin <avagin@openvz.org>
2013-02-27 17:03:12 -0800

    Revert "mm: CMA: import from https://github.com/arco/samsung-kernel-msm7x30/commit/786e7ed1af18679e1adc20d3157f589dd09439f6"
    This reverts commit 4d6debd802bf3ca3c51890310e84568121aa0aa9.
    
    Change-Id: I11cb66be55501a499b2764a68002acd86a19b96c

--- fs/exec.c
+++ fs/exec.c
@@ -1248,7 +1248,7 @@ EXPORT_SYMBOL(install_exec_creds);
 /*
  * determine how safe it is to execute the proposed program
  * - the caller must hold ->cred_guard_mutex to protect against
- *   PTRACE_ATTACH
+ *   PTRACE_ATTACH or seccomp thread-sync
  */
 static int check_unsafe_exec(struct linux_binprm *bprm)
 {
@@ -1353,7 +1353,8 @@ int prepare_binprm(struct linux_binprm *bprm)
 	bprm->cred->euid = current_euid();
 	bprm->cred->egid = current_egid();
 
-	if (!(bprm->file->f_path.mnt->mnt_flags & MNT_NOSUID)) {
+	if (!(bprm->file->f_path.mnt->mnt_flags & MNT_NOSUID) &&
+	    !task_no_new_privs(current)) {
 		/* Set-uid? */
 		if (mode & S_ISUID) {
 			bprm->per_clear |= PER_CLEAR_ON_SETID;
