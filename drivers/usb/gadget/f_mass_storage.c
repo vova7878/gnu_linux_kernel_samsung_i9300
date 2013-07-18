643de63c3167dd5c94d430472f803836f952b502
Andrzej Pietrasiewicz <andrzej.p@samsung.com>
2013-07-18 14:02:28 +0200

    usb/gadget: f_mass_storage: use fsg_common_setup in fsg_common_init
    fsg_common_init is a lengthy function. Now there are helper functions
    which cover all parts of it. Use them.
    
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>

--- drivers/usb/gadget/f_mass_storage.c
+++ drivers/usb/gadget/f_mass_storage.c
@@ -3016,16 +3016,9 @@ struct fsg_common *fsg_common_init(struct fsg_common *common,
 		return ERR_PTR(-EINVAL);
 	}
 
-	/* Allocate? */
-	if (!common) {
-		common = kzalloc(sizeof *common, GFP_KERNEL);
-		if (!common)
-			return ERR_PTR(-ENOMEM);
-		common->free_storage_on_release = 1;
-	} else {
-		memset(common, 0, sizeof *common);
-		common->free_storage_on_release = 0;
-	}
+	common = fsg_common_setup(common, !!common);
+	if (IS_ERR(common))
+		return common;
 	common->sysfs = true;
 	common->state = FSG_STATE_IDLE;
 
@@ -3064,8 +3057,6 @@ struct fsg_common *fsg_common_init(struct fsg_common *common,
 		goto error_release;
 	}
 	common->luns = curlun_it;
-
-	init_rwsem(&common->filesem);
 
 	for (i = 0, lcfg = cfg->luns; i < nluns; ++i, ++curlun_it, ++lcfg) {
 		struct fsg_lun *curlun;
@@ -3166,8 +3157,6 @@ buffhds_first_it:
 	common->can_stall = cfg->can_stall &&
 		!(gadget_is_at91(common->gadget));
 
-	spin_lock_init(&common->lock);
-	kref_init(&common->ref);
 
 	/* Tell the thread to start working */
 	common->thread_task =
@@ -3176,8 +3165,6 @@ buffhds_first_it:
 		rc = PTR_ERR(common->thread_task);
 		goto error_release;
 	}
-	init_completion(&common->thread_notifier);
-	init_waitqueue_head(&common->fsg_wait);
 
 	/* Information */
 	INFO(common, FSG_DRIVER_DESC ", version: " FSG_DRIVER_VERSION "\n");
