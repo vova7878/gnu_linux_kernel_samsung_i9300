1f1e049509e3911d51a7e02d2b2dbe3aaf107d07
Maciej Purski <m.purski@samsung.com>
2017-11-09 11:31:32 +0100

    drm/bridge/sii8620: add DVI mode support
    If the sink device is in HDMI mode, enable infoframe interrupt in scdt
    irq handle function else call start_video function immediately, because
    in DVI mode, there is no infoframe interrupt provided.
    
    Rename start_hdmi function to start_video and get rid of the old
    start_video function. In start_video, if the sink is DVI and mode is
    MHL1 or MHl2, write appropriate values to registers else the path
    should remain the same as in HDMI mode.
    
    Signed-off-by: Maciej Purski <m.purski@samsung.com>
    Change-Id: Iac0f76fd1d974fde8bf5c107b7f63b7aa584198d

--- drivers/gpu/drm/bridge/sil-sii8620.c
+++ drivers/gpu/drm/bridge/sil-sii8620.c
@@ -1116,8 +1116,18 @@ static void sii8620_set_infoframes(struct sii8620 *ctx)
 	sii8620_write_buf(ctx, REG_TPI_INFO_B0, buf, ARRAY_SIZE(buf));
 }
 
-static void sii8620_start_hdmi(struct sii8620 *ctx)
+static void sii8620_start_video(struct sii8620 *ctx)
 {
+	if (ctx->mode < CM_MHL3)
+		sii8620_stop_video(ctx);
+
+	if (ctx->sink_type == SINK_DVI && ctx->mode < CM_MHL3) {
+		sii8620_write(ctx, REG_RX_HDMI_CTRL2,
+			      VAL_RX_HDMI_CTRL2_DEFVAL);
+		sii8620_write(ctx, REG_TPI_SC, 0);
+		return;
+	}
+
 	sii8620_write_seq_static(ctx,
 		REG_RX_HDMI_CTRL2, VAL_RX_HDMI_CTRL2_DEFVAL
 			| BIT_RX_HDMI_CTRL2_USE_AV_MUTE,
@@ -1174,21 +1184,6 @@ static void sii8620_start_hdmi(struct sii8620 *ctx)
 	}
 
 	sii8620_set_infoframes(ctx);
-}
-
-static void sii8620_start_video(struct sii8620 *ctx)
-{
-	if (ctx->mode < CM_MHL3)
-		sii8620_stop_video(ctx);
-
-	switch (ctx->sink_type) {
-	case SINK_HDMI:
-		sii8620_start_hdmi(ctx);
-		break;
-	case SINK_DVI:
-	default:
-		break;
-	}
 }
 
 static void sii8620_disable_hpd(struct sii8620 *ctx)
@@ -1867,8 +1862,13 @@ static void sii8620_irq_scdt(struct sii8620 *ctx)
 	if (stat & BIT_INTR_SCDT_CHANGE) {
 		u8 cstat = sii8620_readb(ctx, REG_TMDS_CSTAT_P3);
 
-		if (cstat & BIT_TMDS_CSTAT_P3_SCDT)
-			sii8620_scdt_high(ctx);
+		if (cstat & BIT_TMDS_CSTAT_P3_SCDT) {
+			if (ctx->sink_type == SINK_HDMI)
+				/* enable infoframe interrupt */
+				sii8620_scdt_high(ctx);
+			else
+				sii8620_start_video(ctx);
+		}
 	}
 
 	sii8620_write(ctx, REG_INTR5, stat);
