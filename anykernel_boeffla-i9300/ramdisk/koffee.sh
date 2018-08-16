a2dc16c57ad504421323362ad067356fe4309336
Jaroslav Zvezda <acroreiser@gmail.com>
2018-08-16 19:25:59 +0300

    koffee.sh: add koffee-rapid-boot and restorecon-on-boot addons
    Change-Id: Ib4446bbe3d1144096984b08f4762824a0bb65971

--- anykernel_boeffla-i9300/ramdisk/koffee.sh
+++ anykernel_boeffla-i9300/ramdisk/koffee.sh
@@ -8,6 +8,14 @@
 /sbin/busybox mount -o bind /res/koffee/hwcomposer.exynos4.so /system/lib/hw/hwcomposer.exynos4.so
 
 /sbin/busybox mount -o remount,rw /
+
+# 2. Pyramid
+/sbin/busybox echo "pyramid" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+
+# 3. SELinux contexts
+/system/bin/restorecon -FRD /data/data
+#
 /sbin/busybox rm /koffee.sh
+/sbin/busybox mount -o remount,ro /libs
 /sbin/busybox mount -o remount,ro /
 /system/bin/toybox setenforce 1
