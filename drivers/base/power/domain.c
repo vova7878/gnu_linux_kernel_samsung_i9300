5902f2132205fec4611faf785588c569dd637191
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-09 19:53:57 +0300

    Revert "PM / Domains: Fix pm_genpd_poweron()"
    This reverts commit fe202fde50a986a8510c62a76dc8733c1a8fac86.

--- drivers/base/power/domain.c
+++ drivers/base/power/domain.c
@@ -80,6 +80,7 @@ static void genpd_set_active(struct generic_pm_domain *genpd)
 int pm_genpd_poweron(struct generic_pm_domain *genpd)
 {
 	struct generic_pm_domain *parent = genpd->parent;
+	DEFINE_WAIT(wait);
 	int ret = 0;
 
  start:
@@ -111,7 +112,7 @@ int pm_genpd_poweron(struct generic_pm_domain *genpd)
 	}
 
 	if (genpd->power_on) {
-		ret = genpd->power_on(genpd);
+		int ret = genpd->power_on(genpd);
 		if (ret)
 			goto out;
 	}
