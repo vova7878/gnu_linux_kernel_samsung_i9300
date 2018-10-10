05c6438eef4cb6382ad72183e51be3d64e9aab1d
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-10 14:46:48 +0300

    Revert "PM Sleep: Do not extend wakeup paths to devices with ignore_children set"
    This reverts commit 8b258cc8ac229aa7d5dcb7cc34cb35d9124498ac.

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -920,8 +920,7 @@ static int __device_suspend(struct device *dev, pm_message_t state, bool async)
  End:
 	if (!error) {
 		dev->power.is_suspended = true;
-		if (dev->power.wakeup_path
-		    && dev->parent && !dev->parent->power.ignore_children)
+		if (dev->power.wakeup_path && dev->parent)
 			dev->parent->power.wakeup_path = true;
 	}
 
