4e224bb8d6e29a3a086f76ad81c5cece92786a44
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-10 14:49:53 +0300

    Revert "regmap: A cache type of _NONE behaves like a bypassed cache"
    This reverts commit e7a6db30df42234bc0f7b9a0af402838e0f146b1.

--- drivers/base/regmap/regcache.c
+++ drivers/base/regmap/regcache.c
@@ -82,10 +82,8 @@ int regcache_init(struct regmap *map)
 	int i;
 	void *tmp_buf;
 
-	if (map->cache_type == REGCACHE_NONE) {
-		map->cache_bypass = true;
+	if (map->cache_type == REGCACHE_NONE)
 		return 0;
-	}
 
 	for (i = 0; i < ARRAY_SIZE(cache_types); i++)
 		if (cache_types[i]->type == map->cache_type)
