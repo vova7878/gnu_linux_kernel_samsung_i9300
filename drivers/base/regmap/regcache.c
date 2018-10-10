8270e2cfb1dfde4b7cb592ce2ae2375f4cac2bd5
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-10 14:48:34 +0300

    Revert "regmap: Grab the lock in regcache_cache_only()"
    This reverts commit 38f6916976f9e964748f097be3688e334fb60f3d.

--- drivers/base/regmap/regcache.c
+++ drivers/base/regmap/regcache.c
@@ -273,9 +273,7 @@ EXPORT_SYMBOL_GPL(regcache_sync);
  */
 void regcache_cache_only(struct regmap *map, bool enable)
 {
-	mutex_lock(&map->sync_lock);
 	map->cache_only = enable;
-	mutex_unlock(&map->sync_lock);
 }
 EXPORT_SYMBOL_GPL(regcache_cache_only);
 
