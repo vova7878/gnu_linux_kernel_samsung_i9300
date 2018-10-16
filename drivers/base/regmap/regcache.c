0a8788643092f556c6ec9051ab7bf74f17971133
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-16 17:48:40 +0300

    Revert "regmap: Move initialization of regcache related fields to regcache_init"
    This reverts commit e5e3b8abeda1cf45f5a079458dbc267952694c7a.

--- drivers/base/regmap/regcache.c
+++ drivers/base/regmap/regcache.c
@@ -87,7 +87,7 @@ err_free:
 	return ret;
 }
 
-int regcache_init(struct regmap *map, const struct regmap_config *config)
+int regcache_init(struct regmap *map)
 {
 	int ret;
 	int i;
@@ -107,13 +107,6 @@ int regcache_init(struct regmap *map, const struct regmap_config *config)
 			map->cache_type);
 		return -EINVAL;
 	}
-
-	map->reg_defaults = config->reg_defaults;
-	map->num_reg_defaults = config->num_reg_defaults;
-	map->num_reg_defaults_raw = config->num_reg_defaults_raw;
-	map->reg_defaults_raw = config->reg_defaults_raw;
-	map->cache_size_raw = (config->val_bits / 8) * config->num_reg_defaults_raw;
-	map->cache_word_size = config->val_bits / 8;
 
 	map->cache = NULL;
 	map->cache_ops = cache_types[i];
