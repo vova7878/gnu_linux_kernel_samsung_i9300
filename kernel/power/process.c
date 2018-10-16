fb8cdbd4387b43e22781e573cd1d66fec23d291c
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-16 17:48:12 +0300

    Revert "freezer: don't distinguish nosig tasks on thaw"
    This reverts commit 6cd8dedcdd8e8de01391a7cf25f0b2afeb24f8f4.

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -170,19 +170,16 @@ int freeze_kernel_threads(void)
 	return error;
 }
 
-void thaw_processes(void)
+static void thaw_tasks(bool nosig_only)
 {
 	struct task_struct *g, *p;
-
-	oom_killer_enable();
-
-	printk("Restarting tasks ... ");
-
-	thaw_workqueues();
 
 	read_lock(&tasklist_lock);
 	do_each_thread(g, p) {
 		if (!freezable(p))
+			continue;
+
+		if (nosig_only && should_send_signal(p))
 			continue;
 
 		if (cgroup_freezing_or_frozen(p))
@@ -191,7 +188,16 @@ void thaw_processes(void)
 		__thaw_task(p);
 	} while_each_thread(g, p);
 	read_unlock(&tasklist_lock);
+}
 
+void thaw_processes(void)
+{
+	oom_killer_enable();
+
+	printk("Restarting tasks ... ");
+	thaw_workqueues();
+	thaw_tasks(true);
+	thaw_tasks(false);
 	schedule();
 	printk("done.\n");
 }
