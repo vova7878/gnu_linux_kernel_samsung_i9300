7abf31de0247323b8bece65714850ae14186d9a8
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-16 17:35:11 +0300

    Revert "PM / Sleep: Simplify generic system suspend callbacks"
    This reverts commit 8ca6d9bcc8d33c592c0855b4b1481bc723ac7e85.

--- drivers/base/power/generic_ops.c
+++ drivers/base/power/generic_ops.c
@@ -97,16 +97,16 @@ int pm_generic_prepare(struct device *dev)
  * @event: PM transition of the system under way.
  * @bool: Whether or not this is the "noirq" stage.
  *
- * Execute the suspend/freeze/poweroff/thaw callback provided by the driver of
- * @dev, if defined, and return its error code.    Return 0 if the callback is
- * not present.
+ * If the device has not been suspended at run time, execute the
+ * suspend/freeze/poweroff/thaw callback provided by its driver, if defined, and
+ * return its error code.  Otherwise, return zero.
  */
 static int __pm_generic_call(struct device *dev, int event, bool noirq)
 {
 	const struct dev_pm_ops *pm = dev->driver ? dev->driver->pm : NULL;
 	int (*callback)(struct device *);
 
-	if (!pm)
+	if (!pm || pm_runtime_suspended(dev))
 		return 0;
 
 	switch (event) {
@@ -217,12 +217,14 @@ EXPORT_SYMBOL_GPL(pm_generic_thaw);
  * @bool: Whether or not this is the "noirq" stage.
  *
  * Execute the resume/resotre callback provided by the @dev's driver, if
- * defined, and return its error code.  Return 0 if the callback is not present.
+ * defined.  If it returns 0, change the device's runtime PM status to 'active'.
+ * Return the callback's error code.
  */
 static int __pm_generic_resume(struct device *dev, int event, bool noirq)
 {
 	const struct dev_pm_ops *pm = dev->driver ? dev->driver->pm : NULL;
 	int (*callback)(struct device *);
+	int ret;
 
 	if (!pm)
 		return 0;
@@ -239,7 +241,17 @@ static int __pm_generic_resume(struct device *dev, int event, bool noirq)
 		break;
 	}
 
-	return callback ? callback(dev) : 0;
+	if (!callback)
+		return 0;
+
+	ret = callback(dev);
+	if (!ret && !noirq && pm_runtime_enabled(dev)) {
+		pm_runtime_disable(dev);
+		pm_runtime_set_active(dev);
+		pm_runtime_enable(dev);
+	}
+
+	return ret;
 }
 
 /**
