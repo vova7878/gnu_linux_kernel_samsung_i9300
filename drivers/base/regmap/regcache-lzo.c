c6ca81e97e9f9d2f0e1dc3e23d8cfff04e49e35b
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-16 17:48:49 +0300

    Revert "regmap: Prepare LZO cache for variable block sizes"
    This reverts commit 82732bdd663ee9dc1ad4b0409881fe89a9d827ca.

--- drivers/base/regmap/regcache-lzo.c
+++ drivers/base/regmap/regcache-lzo.c
@@ -27,7 +27,7 @@ struct regcache_lzo_ctx {
 };
 
 #define LZO_BLOCK_NUM 8
-static int regcache_lzo_block_count(struct regmap *map)
+static int regcache_lzo_block_count(void)
 {
 	return LZO_BLOCK_NUM;
 }
@@ -106,22 +106,19 @@ static inline int regcache_lzo_get_blkindex(struct regmap *map,
 					    unsigned int reg)
 {
 	return (reg * map->cache_word_size) /
-		DIV_ROUND_UP(map->cache_size_raw,
-			     regcache_lzo_block_count(map));
+		DIV_ROUND_UP(map->cache_size_raw, regcache_lzo_block_count());
 }
 
 static inline int regcache_lzo_get_blkpos(struct regmap *map,
 					  unsigned int reg)
 {
-	return reg % (DIV_ROUND_UP(map->cache_size_raw,
-				   regcache_lzo_block_count(map)) /
+	return reg % (DIV_ROUND_UP(map->cache_size_raw, regcache_lzo_block_count()) /
 		      map->cache_word_size);
 }
 
 static inline int regcache_lzo_get_blksize(struct regmap *map)
 {
-	return DIV_ROUND_UP(map->cache_size_raw,
-			    regcache_lzo_block_count(map));
+	return DIV_ROUND_UP(map->cache_size_raw, regcache_lzo_block_count());
 }
 
 static int regcache_lzo_init(struct regmap *map)
@@ -134,7 +131,7 @@ static int regcache_lzo_init(struct regmap *map)
 
 	ret = 0;
 
-	blkcount = regcache_lzo_block_count(map);
+	blkcount = regcache_lzo_block_count();
 	map->cache = kzalloc(blkcount * sizeof *lzo_blocks,
 			     GFP_KERNEL);
 	if (!map->cache)
@@ -206,7 +203,7 @@ static int regcache_lzo_exit(struct regmap *map)
 	if (!lzo_blocks)
 		return 0;
 
-	blkcount = regcache_lzo_block_count(map);
+	blkcount = regcache_lzo_block_count();
 	/*
 	 * the pointer to the bitmap used for syncing the cache
 	 * is shared amongst all lzo_blocks.  Ensure it is freed
