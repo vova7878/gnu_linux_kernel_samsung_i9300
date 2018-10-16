25bdf9e67a2b6e96163bb08669e017c934e21986
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-16 17:36:26 +0300

    Revert "PM / Domains: Make it possible to assign names to generic PM domains"
    This reverts commit e84b2c202771bbd538866207efcb1f7dbab8045b.
    
    Change-Id: Iff081afd407af1a963b67eac564badd6f4bedf67

--- include/linux/pm_domain.h
+++ include/linux/pm_domain.h
@@ -50,7 +50,6 @@ struct generic_pm_domain {
 	struct mutex lock;
 	struct dev_power_governor *gov;
 	struct work_struct power_off_work;
-	char *name;
 	unsigned int in_progress;	/* Number of devices being suspended now */
 	atomic_t sd_count;	/* Number of subdomains with power "on" */
 	enum gpd_status status;	/* Current state of the domain */
