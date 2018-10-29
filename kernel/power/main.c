34f40b30dedb36af496b4ced5da4414551856e09
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-29 23:25:45 +0300

    Revert "PM / Suspend: Avoid code duplication in suspend statistics update"
    This reverts commit 8916e3702ec422b57cc549fbae3986106292100f.

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -296,7 +296,11 @@ static ssize_t state_store(struct kobject *kobj, struct kobj_attribute *attr,
 	}
 	if (state < PM_SUSPEND_MAX && *s) {
 		error = enter_state(state);
-		suspend_stats_update(error);
+		if (error) {
+			suspend_stats.fail++;
+			dpm_save_failed_errno(error);
+		} else
+			suspend_stats.success++;
 	}
 #endif
 
