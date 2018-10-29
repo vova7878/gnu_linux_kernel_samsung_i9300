2ae517d5fa182ff3750958595d0387c0443b63ff
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-30 00:08:45 +0300

    power
    Change-Id: I4512e4e4484f266edc76ba145f335fa3c1251b87

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -16,6 +16,7 @@
 #include <linux/freezer.h>
 #include <linux/delay.h>
 #include <linux/workqueue.h>
+#include <linux/wakelock.h>
 
 /* 
  * Timeout for stopping processes
@@ -82,6 +83,10 @@ static int try_to_freeze_tasks(bool sig_only)
 			todo += wq_busy;
 		}
 
+		if (todo && has_wake_lock(WAKE_LOCK_SUSPEND)) {
+			wakeup = 1;
+			break;
+		}
 		if (!todo || time_after(jiffies, end_time))
 			break;
 
