bfe9125ca83e3821ee11164ec3c508e025ab08e6
Shilin Victor <chrono.monochrome@gmail.com>
2018-11-09 00:07:46 +0300

    namespace: fix merge derp
    Change-Id: Ibfbd6a2d5d7153e160ac7614cb45178b045f5fef

--- fs/namespace.c
+++ fs/namespace.c
@@ -480,42 +480,6 @@ int sb_prepare_remount_readonly(struct super_block *sb)
 	return err;
 }
 
-int sb_prepare_remount_readonly(struct super_block *sb)
-{
-	struct mount *mnt;
-	int err = 0;
-
-	/* Racy optimization.  Recheck the counter under MNT_WRITE_HOLD */
-	if (atomic_long_read(&sb->s_remove_count))
-		return -EBUSY;
-
-	br_write_lock(vfsmount_lock);
-	list_for_each_entry(mnt, &sb->s_mounts, mnt_instance) {
-		if (!(mnt->mnt.mnt_flags & MNT_READONLY)) {
-			mnt->mnt.mnt_flags |= MNT_WRITE_HOLD;
-			smp_mb();
-			if (mnt_get_writers(mnt) > 0) {
-				err = -EBUSY;
-				break;
-			}
-		}
-	}
-	if (!err && atomic_long_read(&sb->s_remove_count))
-		err = -EBUSY;
-
-	if (!err) {
-		sb->s_readonly_remount = 1;
-		smp_wmb();
-	}
-	list_for_each_entry(mnt, &sb->s_mounts, mnt_instance) {
-		if (mnt->mnt.mnt_flags & MNT_WRITE_HOLD)
-			mnt->mnt.mnt_flags &= ~MNT_WRITE_HOLD;
-	}
-	br_write_unlock(vfsmount_lock);
-
-	return err;
-}
-
 static void free_vfsmnt(struct mount *mnt)
 {
 	kfree(mnt->mnt_devname);
