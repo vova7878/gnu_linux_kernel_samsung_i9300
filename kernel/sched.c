4af4f523da10b3c4962bdd67a2c6826e711d58b3
Shilin Victor <chrono.monochrome@gmail.com>
2018-11-12 03:32:14 +0300

    Revert "sched: prio_experiment: RT instead of higher io prio, again"
    This reverts commit 78c1acfed19d84571873244e98551cbe46f32ce9.

--- kernel/sched.c
+++ kernel/sched.c
@@ -5116,7 +5116,7 @@ void set_user_nice(struct task_struct *p, long nice)
 #if defined (CONFIG_IO_PRIO_BOOST)
 	if (nice == -10 && TASK_NICE(p) == 0 && p->cred->uid > 10000)
 	{
-		set_task_ioprio(p, IOPRIO_PRIO_VALUE(1,6));
+		set_task_ioprio(p, IOPRIO_PRIO_VALUE(0,2));
 	}
 	else if (TASK_NICE(p) == -10 && nice == 0 && p->cred->uid > 10000)
 	{	
