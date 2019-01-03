e8aff362f109771786a038265f5132b09efeaf84
Shilin Victor <chrono.monochrome@gmail.com>
2019-01-03 15:03:20 +0300

    Revert "mm: CMA: import from https://github.com/arco/samsung-kernel-msm7x30/commit/786e7ed1af18679e1adc20d3157f589dd09439f6"
    This reverts commit a1544033e324572fc3c1bd8735c671844e3935b4.

--- include/linux/migrate.h
+++ include/linux/migrate.h
@@ -39,9 +39,19 @@ extern int migrate_huge_page_move_mapping(struct address_space *mapping,
 #else
 
 static inline void putback_lru_pages(struct list_head *l) {}
+#ifndef CONFIG_DMA_CMA
 static inline int migrate_pages(struct list_head *l, new_page_t x,
 		unsigned long private, bool offlining,
 		enum migrate_mode mode) { return -ENOSYS; }
+#else
+static inline int migrate_pages(struct list_head *l, new_page_t x,
+		unsigned long private, bool offlining,
+		enum migrate_mode mode, int tries) { return -ENOSYS; }
+
+static inline int migrate_replace_cma_page(struct page *oldpage,
+		struct page **newpage) { return -ENOSYS; }
+#endif
+
 static inline int migrate_huge_pages(struct list_head *l, new_page_t x,
 		unsigned long private, bool offlining,
 		enum migrate_mode mode) { return -ENOSYS; }
