e6deea9bd8dd3d15cc0100dd64557162d428becf
Shilin Victor <chrono.monochrome@gmail.com>
2019-02-12 22:44:27 +0300

    Revert "PM / Freezer: Thaw only kernel threads if freezing of kernel threads fails"
    This reverts commit 2eb10f78c2c41dce405e337932d7bed9537ce23c.

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -148,10 +148,7 @@ int freeze_processes(void)
 /**
  * freeze_kernel_threads - Make freezable kernel threads go to the refrigerator.
  *
- * On success, returns 0.  On failure, -errno and only the kernel threads are
- * thawed, so as to give a chance to the caller to do additional cleanups
- * (if any) before thawing the userspace tasks. So, it is the responsibility
- * of the caller to thaw the userspace tasks, when the time is right.
+ * On success, returns 0.  On failure, -errno and system is fully thawed.
  */
 int freeze_kernel_threads(void)
 {
@@ -167,7 +164,7 @@ int freeze_kernel_threads(void)
 	BUG_ON(in_atomic());
 
 	if (error)
-		thaw_kernel_threads();
+		thaw_processes();
 	return error;
 }
 
