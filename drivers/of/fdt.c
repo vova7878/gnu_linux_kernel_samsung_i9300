0aefef6ff66f4660c93335b55fcfa970bb27950c
Shilin Victor <radicaldreamer00001@gmail.com>
2019-05-11 22:40:03 +0300

    Revert "fdt: fix extend of cmd line"
    This reverts commit f67c6ff5b9fd8096b3607520cb4cae5562f42eb2.

--- drivers/of/fdt.c
+++ drivers/of/fdt.c
@@ -844,16 +844,10 @@ int __init early_init_dt_scan_chosen(unsigned long node, const char *uname,
 	 * is set in which case we override whatever was found earlier.
 	 */
 #ifdef CONFIG_CMDLINE
-#if defined(CONFIG_CMDLINE_EXTEND)
-	strlcat(data, " ", COMMAND_LINE_SIZE);
-	strlcat(data, CONFIG_CMDLINE, COMMAND_LINE_SIZE);
-#elif defined(CONFIG_CMDLINE_FORCE)
-	strlcpy(data, CONFIG_CMDLINE, COMMAND_LINE_SIZE);
-#else
-	/* No arguments from boot loader, use kernel's  cmdl*/
+#ifndef CONFIG_CMDLINE_FORCE
 	if (!((char *)data)[0])
-		strlcpy(data, CONFIG_CMDLINE, COMMAND_LINE_SIZE);
 #endif
+		strlcpy(data, CONFIG_CMDLINE, COMMAND_LINE_SIZE);
 #endif /* CONFIG_CMDLINE */
 
 	pr_debug("Command line is: %s\n", (char*)data);
