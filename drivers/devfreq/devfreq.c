b97c9b930f0bf88c75da1887f1b06568fa4814f7
Shilin Victor <chrono.monochrome@gmail.com>
2019-09-23 23:28:39 +0300

    Revert "PM / devfreq: fix use after free in devfreq_remove_device"
    This reverts commit 9f3bdd4f937a75c4589a867dc1f8fefe09c1a618.

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -418,14 +418,10 @@ out:
  */
 int devfreq_remove_device(struct devfreq *devfreq)
 {
-	bool central_polling;
-
 	if (!devfreq)
 		return -EINVAL;
 
-	central_polling = !devfreq->governor->no_central_polling;
-
-	if (central_polling) {
+	if (!devfreq->governor->no_central_polling) {
 		mutex_lock(&devfreq_list_lock);
 		while (wait_remove_device == devfreq) {
 			mutex_unlock(&devfreq_list_lock);
@@ -437,7 +433,7 @@ int devfreq_remove_device(struct devfreq *devfreq)
 	mutex_lock(&devfreq->lock);
 	_remove_devfreq(devfreq, false); /* it unlocks devfreq->lock */
 
-	if (central_polling)
+	if (!devfreq->governor->no_central_polling)
 		mutex_unlock(&devfreq_list_lock);
 
 	return 0;
