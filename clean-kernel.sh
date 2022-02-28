0e3ec30f4ca5059a348d21af0117ec88049fa27c
vova7878 <vova20017878@gmail.com>
2022-02-28 17:34:29 +0700

    i9300: Add new linux_i9300_defconfig

--- clean-kernel.sh
+++ clean-kernel.sh
@@ -1,3 +1,3 @@
 cp .config .config.bkp
-make ARCH=arm CROSS_COMPILE=/android-kernel/ICS/toolchain/bin/arm-none-eabi- mrproper
+make ARCH=arm CROSS_COMPILE=/home/vova7878/i9300_kernel/gcc-linaro-7.5.0-2019.12-x86_64_arm-eabi/bin/arm-eabi- mrproper
 cp .config.bkp .config
